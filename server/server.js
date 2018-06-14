57d3f3fd6ee854a1a45dbc0210842519db105ace
pang <pang.li@pkfare.com>
2018-06-14 20:54:17 +0800

    axios异步，proxy代理

--- server/server.js
+++ server/server.js
@@ -8,17 +8,40 @@ mongoose.connection.on('connected', function () {
     console.log('mongo connect sucess');
 });
 
+const User = mongoose.model('user', new mongoose.Schema({
+    // user: {type: String, required: true},
+    // age: {type: Number, required: true}
+}))
+
+// 新增数据
+// User.create({
+// 	user:'xiaohua',
+// 	age:12
+// },function(err, doc){
+// 	if (!err) {
+// 		console.log(doc)
+// 	}else{
+// 		console.log(err)
+// 	}
+// })
 // 新建app
+// User.remove({age:18},function(err,doc){
+// 	console.log(doc)
+// })
+// User.update({'user':'xiaoming'},{'$set':{age:26}},function(err,doc){
+// 	console.log(doc)
+// })
 const app = express();
-
-
 app.get('/', function (req, res) {
     res.send('<h1>hello world</h1>');
 });
+
 app.get('/data', function (req, res) {
-    res.json({a: 333, ll: 'eee333'});
+    User.findOne({}, function (err, doc) {
+        res.json(doc)
+    })
 });
 
-app.listen(9005, function () {
+app.listen(9093, function () {
     console.log('NODE app start at port 9005');
 });
--- src/Auth.js
+++ src/Auth.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
-import {login} from './Auth.redux';
+import {login, getUserData} from './Auth.redux';
+import axios from 'axios';
 
 @connect(
     state => state.auth,
-    {login}
+    {login, getUserData}
 )
 
 export default class Auth extends React.Component {
@@ -14,9 +15,14 @@ export default class Auth extends React.Component {
         super(props);
     }
 
+    componentDidMount() {
+        this.props.getUserData();
+    }
+
     render() {
         return (
             <div>
+                <h2>我的名字是{this.props.user}, 年龄：{this.props.age}</h2>
                 {this.props.isAuth ? <Redirect to="/dashboard"/> : null}
                 <h2>你没有权限，请登录</h2>
                 <button onClick={this.props.login}>登录</button>
