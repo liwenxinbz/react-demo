b43395ac363bb1c8b26c4eea7bd292ae3f9f05e6
pang <pang.li@pkfare.com>
2018-06-14 16:33:58 +0800

    redux和配置redux插件

--- src/App.js
+++ src/App.js
@@ -1,71 +1,18 @@
-import React, {Component} from 'react';
-// import {Button} from 'antd-mobile';
-// import logo from './logo.svg';
-// import './App.css';
-// import 'antd-mobile/dist/antd-mobile.css';
-//
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React111</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//           <Button type="primary">我是按钮</Button>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
+import React from 'react';
 
+export default class App extends React.Component {
 
-export default class App extends Component {
     render() {
-        const boss = '李云龙';
+        const store = this.props.store;
+        const num = store.getState();
+        const {addGUN, removeGUN, addGunAsync} = this.props
         return (
             <div>
-                <h2>{boss}</h2>
-                <Yiying 老大="张大毛"></Yiying>
-                <Qibinglian laoda="宋德胜"/>
+                <h2>{`现在有机枪${num}把`}</h2>
+                <button onClick={() => store.dispatch(addGUN())}>申请武器</button>
+                <button onClick={() => store.dispatch(removeGUN())}>上交武器</button>
+                <button onClick={() => store.dispatch(addGunAsync())}>拖两天再给</button>
             </div>
         );
     }
-}
-
-function Qibinglian(props) {
-    return <h2>骑兵连连长： {props.laoda}</h2>
-}
-
-class Yiying extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            solders: ['胡子', '柱子', '王根生']
-        };
-    }
-
-    addSolder = () => {
-        this.setState({solders: [...this.state.solders, '新兵蛋子' + Math.random()]})
-    };
-
-    render() {
-        return (
-            <div>
-                <h2>一营营长： {this.props.老大}</h2>
-                <button onClick={this.addSolder}>新兵入伍</button>
-                <ul>
-                    {this.state.solders.map(v => {
-                        return <li key={v}>{v}</li>;
-                    })}
-                </ul>
-            </div>
-        );
-    }
-
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDom from 'react-dom';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {counter, addGUN, removeGUN, addGunAsync} from './index.redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const reduxDevtools = window.devToolsExtension ? window.devToolsExtension : () => {};
+const store = createStore(counter, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+function render() {
+    ReactDom.render(
+        <App
+            store={store}
+            addGUN={addGUN}
+            removeGUN={removeGUN}
+            addGunAsync={addGunAsync}
+        />,
+        document.getElementById('root')
+    );
+}
+
+render();
+
+store.subscribe(render);
