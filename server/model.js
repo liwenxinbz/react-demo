03fe757f4d687717c21f38b12feb5d5ad10412e4
pang <pang.li@pkfare.com>
2018-06-15 18:15:27 +0800

    输入库模型建立

--- server/model.js
+++ server/model.js
@@ -1,6 +1,36 @@
 
 const mongoose = require('mongoose');
 
-const DB_URL = 'mongodb://127.0.0.1:27017';
+const DB_URL = 'mongodb://127.0.0.1:27017/imooc-chat';
 
 mongoose.connect(DB_URL);
+
+const models = {
+    user: {
+        "user": {'type': String, 'require': true},
+        "pwd": {'type': String, 'require': true},
+        "type": {'type': String, 'require': true},
+    //    头像
+        "avatar": {'type': String},
+    //    个人简介
+        "desc": {'type': String},
+        // 职位名
+        "title": {'type': String},
+    //    如果是boss
+        "company": {'type': String},
+        "monty": {'type': String}
+    },
+    chat: {
+
+    }
+};
+
+for (let m in models) {
+    mongoose.model(m, new mongoose.Schema(models[m]));
+}
+
+module.exports = {
+    getModel: function (name) {
+        return mongoose.model(name);
+    }
+};
