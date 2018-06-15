ea0677daa1e4f13ecbe45bab9d432a50c304fabc
pang <pang.li@pkfare.com>
2018-06-15 11:48:42 +0800

    拦截器

--- src/config.js
+++ src/config.js
@@ -1,7 +1,20 @@
 import axios from 'axios';
-import {} from 'antd-mobile';
+import {Toast} from 'antd-mobile';
+
+
 //拦截请求；
 
 axios.interceptors.request.use(function (config) {
+    Toast.loading('加载中', 0);
+    console.log(config, 'request');
+    return config;
+});
 
-})
+//拦截响应
+axios.interceptors.response.use(function (config) {
+    console.log(config, 'response');
+    setTimeout(function () {
+       Toast.hide();
+    }, 300);
+    return config;
+});
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 import reducers from './reducer';
 import Auth from './Auth';
 import Dashboard from './Dashboard';
+import './config';
 
 const store = createStore(reducers, compose(
     applyMiddleware(thunk),
